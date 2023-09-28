8afcd5d557eae6d0d19b0498d67a029148a4f78e
ronak raj <126279165+ronakraj00@users.noreply.github.com>
2023-09-29 00:59:12 +0530

    make shimmer code small

--- src/components/shimmer.js
+++ src/components/shimmer.js
@@ -1,65 +1,18 @@
 export default function Shimmer() {
-    return <div className="skeleton">
-        <div className="restaurant-skeleton">
-            <div className="img"></div>
-            <div className="name"></div>
-            <div className="discription"></div>
-            <div className="shimmer"></div>
+    return (
+        <div className="skeleton">
+            {Array(10)
+                .fill("")
+                .map((item) => {
+                    return (
+                        <div className="restaurant-skeleton">
+                            <div className="img"></div>
+                            <div className="name"></div>
+                            <div className="discription"></div>
+                            <div className="shimmer"></div>
+                        </div>
+                    );
+                })}
         </div>
-        <div className="restaurant-skeleton">
-            <div className="img"></div>
-            <div className="name"></div>
-            <div className="discription"></div>
-            <div className="shimmer"></div>
-        </div>
-        <div className="restaurant-skeleton">
-            <div className="img"></div>
-            <div className="name"></div>
-            <div className="discription"></div>
-            <div className="shimmer"></div>
-        </div>
-        <div className="restaurant-skeleton">
-            <div className="img"></div>
-            <div className="name"></div>
-            <div className="discription"></div>
-            <div className="shimmer"></div>
-        </div>
-        <div className="restaurant-skeleton">
-            <div className="img"></div>
-            <div className="name"></div>
-            <div className="discription"></div>
-            <div className="shimmer"></div>
-        </div>
-        <div className="restaurant-skeleton">
-            <div className="img"></div>
-            <div className="name"></div>
-            <div className="discription"></div>
-            <div className="shimmer"></div>
-        </div>
-        <div className="restaurant-skeleton">
-            <div className="img"></div>
-            <div className="name"></div>
-            <div className="discription"></div>
-            <div className="shimmer"></div>
-        </div>
-        <div className="restaurant-skeleton">
-            <div className="img"></div>
-            <div className="name"></div>
-            <div className="discription"></div>
-            <div className="shimmer"></div>
-        </div>
-        <div className="restaurant-skeleton">
-            <div className="img"></div>
-            <div className="name"></div>
-            <div className="discription"></div>
-            <div className="shimmer"></div>
-        </div>
-        <div className="restaurant-skeleton">
-            <div className="img"></div>
-            <div className="name"></div>
-            <div className="discription"></div>
-            <div className="shimmer"></div>
-        </div>
-    </div>
+    );
 }
-
