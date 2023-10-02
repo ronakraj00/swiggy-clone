de3edfc862f2c0f16deb70231dcf54b42c4e7314
ronak raj <126279165+ronakraj00@users.noreply.github.com>
2023-10-02 15:39:19 +0530

    css to header using tailwind

--- src/components/Header.js
+++ src/components/Header.js
@@ -11,32 +11,32 @@ const Header = () => {
     let online = useOnlineStatus();
 
     return (
-        <header>
-            <div className="online-status">{online ? "💚" : "💘"}</div>
-            <a href="/">
+        <header className="box-border flex justify-between p-5 shadow-md">
+            <div className="absolute top-7 left-2">{online ? "💚" : "💘"}</div>
+            <a href="/" className="mx-10">
                 <img src={swiggyLogo} />
             </a>
-            <h4 className="location">Location</h4>
-            <div className="nav">
+            <h4 className="text-gray-900 font-bold font-mono py-2">Location</h4>
+            <div className="  box-border py-2 px-7 fixed left-0 bottom-0 flex justify-between items-center bg-white max-sm:w-full lg:static w-1/2">
                 <Link to={"/search"}>
-                    <img src={searchIcon} alt="" />
-                    <h4 href="">Search</h4>
+                    <img src={searchIcon} className="w-8 py-1"/>
+                    <h4 className="hidden">Search</h4>
                 </Link>
                 <Link to={"/offers"}>
-                    <img src={offersIcon} alt="" />
-                    <h4 href="">Offers</h4>
+                    <img src={offersIcon} className="w-8 py-1" />
+                    <h4 className="hidden">Offers</h4>
                 </Link>
                 <Link to={"/help"}>
-                    <img src={HelpIcon} alt="" />
-                    <h4 href="">Help</h4>
+                    <img src={HelpIcon} className="w-8 py-1"/>
+                    <h4 className="hidden">Help</h4>
                 </Link>
                 <Link to={"/signin"}>
-                    <img src={SignIcon} alt="" />
-                    <h4 href="">Sign In</h4>
+                    <img src={SignIcon} className="w-8 py-1"/>
+                    <h4 className="hidden">Sign In</h4>
                 </Link>
                 <Link to={"/cart"}>
-                    <img src={cartIcon} alt="" />
-                    <h4 href="">Cart</h4>
+                    <img src={cartIcon} className="w-8 py-1"/>
+                    <h4 className="hidden">Cart</h4>
                 </Link>
             </div>
         </header>
